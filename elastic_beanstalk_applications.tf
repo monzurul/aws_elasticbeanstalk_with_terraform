9102b19eb255c00a5e9e617208d975511895e5df
Monzurul Hoque <ziku@escenic.com>
2018-02-27 18:45:28 +0600

    Added EB support

--- elastic_beanstalk_applications.tf
+++ elastic_beanstalk_applications.tf
@@ -78,3 +78,119 @@ resource "aws_subnet" "private_staging_sn_1b" {
   }  
 }
 
+# The elastic beanstalk application
+resource "aws_elastic_beanstalk_application" "staging_application" {
+  name = "StagingApplication"
+  description = "StagingApplication"
+}
+
+# The staging environment
+resource "aws_elastic_beanstalk_environment" "staging_app" {
+  name                  = "stagingapp"
+  application           = "${aws_elastic_beanstalk_application.staging_application.name}"
+  solution_stack_name   = "64bit Amazon Linux 2017.09 v2.6.5 running PHP 7.1"
+  tier                  = "WebServer"
+
+  setting {
+    name = "InstanceType"
+    namespace = "aws:autoscaling:launchconfiguration"
+    value = "t2.micro"
+  }
+
+  setting {
+    name = "MinSize"
+    namespace = "aws:autoscaling:asg"
+    value = "1"
+  }
+  setting {
+    name = "MaxSize"
+    namespace = "aws:autoscaling:asg"
+    value = "4"
+  }
+
+  # This is the VPC that the instances will use.
+  setting {
+    namespace = "aws:ec2:vpc"
+    name      = "VPCId"
+    value     = "${aws_vpc.staging_vpc.id}"
+  }
+
+  # This is the subnet of the ELB
+  setting {
+    namespace = "aws:ec2:vpc"
+    name      = "ELBSubnets"
+    value     = "${aws_subnet.public_staging_sn_1a.id}"
+  }
+
+  # This is the subnets for the instances.
+  setting {
+    namespace = "aws:ec2:vpc"
+    name      = "Subnets"
+    value     = "${aws_subnet.public_staging_sn_1a.id}"
+  }
+
+  # You can set the environment type, single or LoadBalanced
+  setting {
+    namespace = "aws:elasticbeanstalk:environment"
+    name      = "EnvironmentType"
+    value     = "LoadBalanced"
+  }
+
+  # Example of setting environment variables
+  setting {
+    namespace = "aws:elasticbeanstalk:application:environment"
+    name      = "AWS_ENVIRONMENT"
+    value     = "staging"
+  }
+
+  setting {
+    name = "Availability Zones"
+    namespace = "aws:autoscaling:asg"
+    value = "Any 2"
+  }
+
+  # Are the load balancers multizone?
+  setting {
+    namespace = "aws:elb:loadbalancer"
+    name      = "CrossZone"
+    value     = "true"
+  }
+
+  # Enable connection draining.
+  setting {
+    namespace = "aws:elb:policies"
+    name      = "ConnectionDrainingEnabled"
+    value     = "true"
+  }
+
+  setting {
+    name = "Unit"
+    namespace = "aws:autoscaling:trigger"
+    value = "Percent"
+  }
+  setting {
+    name = "MeasureName"
+    namespace = "aws:autoscaling:trigger"
+    value = "CPUUtilization"
+  }
+  setting {
+    name = "LowerThreshold"
+    namespace = "aws:autoscaling:trigger"
+    value = "40"
+  }
+  setting {
+    name = "UpperThreshold"
+    namespace = "aws:autoscaling:trigger"
+    value = "80"
+  }
+  setting {
+    name = "UpperBreachScaleIncrement"
+    namespace = "aws:autoscaling:trigger"
+    value = "1"
+  }
+  setting {
+    name = "LowerBreachScaleIncrement"
+    namespace = "aws:autoscaling:trigger"
+    value = "-1"
+  }
+}
